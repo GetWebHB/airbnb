2dfd311be002aae9af310da43f7ca10da96623b9
GetWebHB <1597834867@qq.com>
2022-12-23 22:54:57 +0800

    feat: banner

---
+++ react-airbnb/src/views/Home/c-cpns/Banner/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import Bg from '@/assets/img/banner.jpg'
+
+const BannerWrapper = styled.div`
+  width: 100%;
+  height: 529px;
+  background: url(${Bg}) center no-repeat;
+  background-size: cover;
+`
+
+export default BannerWrapper
--- react-airbnb/src/views/Home/index.tsx
+++ react-airbnb/src/views/Home/index.tsx
@@ -3,6 +3,9 @@ import { useSelector, shallowEqual, useDispatch } from 'react-redux'
 import { increment, decrement } from '@/store/features/counterSlice'
 import type { RootState } from '@/store'
 
+import Banner from './c-cpns/Banner'
+import HomeWrapper from './style'
+
 const Home = memo(() => {
   const { counter } = useSelector(
     (state: RootState) => ({
@@ -13,11 +16,9 @@ const Home = memo(() => {
   const dispatch = useDispatch()
 
   return (
-    <div>
-      <h1>{counter}</h1>
-      <button onClick={() => dispatch(increment(10))}>+10</button>
-      <button onClick={() => dispatch(decrement(10))}>-10</button>
-    </div>
+    <HomeWrapper>
+      <Banner />
+    </HomeWrapper>
   )
 })
 
